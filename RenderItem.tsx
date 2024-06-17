846ca5ea8a2838ccecde7f2d9065b2a69f327820
GaboDev <gabodevcol@gmail.com>
2024-06-17 17:14:55 -0500

    Function to delete task

--- RenderItem.tsx
+++ RenderItem.tsx
@@ -1,29 +1,35 @@
-import React from "react";
-import styles from "./Styles";
-import { Text, TouchableOpacity, View } from "react-native";
- import { Task} from './App'
+import React from 'react';
+import styles from './Styles';
+import {Text, TouchableOpacity, View} from 'react-native';
+import {Task} from './App';
 
 interface ItemProps {
-    item: Task;
-    markDone: (task : Task)=> void ;
-    deleteFunction:()=> void;
+  item: Task;
+  markDone: (task: Task) => void;
+  deleteFunction: (task: Task) => void;
 }
-export default function RenderItem({item, markDone, deleteFunction}:ItemProps) {
-    return (
-      <View style={styles.itemContainer}>
-        <TouchableOpacity onPress={()=>markDone(item)}>
-          <Text style={item.done ? styles.textDone : styles.text}>
-            {item.title}
-          </Text>
-          <Text style={item.done ? styles.textDone : styles.text}>
-            {item.date.toLocaleDateString()}
-          </Text>
+export default function RenderItem({
+  item,
+  markDone,
+  deleteFunction,
+}: ItemProps) {
+  return (
+    <View style={styles.itemContainer}>
+      <TouchableOpacity onPress={() => markDone(item)}>
+        <Text style={item.done ? styles.textDone : styles.text}>
+          {item.title}
+        </Text>
+        <Text style={item.done ? styles.textDone : styles.text}>
+          {item.date.toLocaleDateString()}
+        </Text>
+      </TouchableOpacity>
+      {item.done && (
+        <TouchableOpacity
+          style={styles.removeButton}
+          onPress={() => deleteFunction(item)}>
+          <Text style={styles.whiteText}>Eliminar</Text>
         </TouchableOpacity>
-        {item.done && (
-          <TouchableOpacity onPress={deleteFunction} style={styles.removeButton}>
-            <Text style={styles.whiteText}>Eliminar</Text>
-          </TouchableOpacity>
-        )}
-      </View>
-    );
-  }
+      )}
+    </View>
+  );
+}
