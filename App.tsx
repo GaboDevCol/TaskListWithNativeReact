f059637381c89474c02f0732fbd94f0e1e589188
GaboDev <gabodevcol@gmail.com>
2024-06-17 16:43:25 -0500

    Function to add new task

--- App.tsx
+++ App.tsx
@@ -1,25 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, TextInput, TouchableOpacity, FlatList} from 'react-native';
 import styles from './Styles';
 import RenderItem from './RenderItem';
-
-const tasks = [
-  {
-    title: 'Alimentar al perro',
-    done: false,
-    date: new Date(),
-  },
-  {
-    title: 'Salir a correr',
-    done: false,
-    date: new Date(),
-  },
-  {
-    title: 'Hacer la tarea',
-    done: true,
-    date: new Date(),
-  },
-];
 
 export interface Task {
   title: string;
@@ -28,26 +10,52 @@ export interface Task {
 }
 
 export default function App() {
-  const markDone = () => {
-    console.log('markDone')
+  const [text, setText] = useState('');
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = () => {
+    const tmp = [...tasks];
+    const newTask = {
+      title: text,
+      done: false,
+      date: new Date(),
+    };
+    tmp.push(newTask);
+
+    setTasks(tmp);
+    setText('');
   };
+
+  const markDone = () => {
+    console.log('markDone');
+  };
+
   const deleteFunction = () => {
-    console.log('delete')
+    console.log('delete');
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Mis tareas por hacer.</Text>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Agrega nueva tarea" style={styles.textInput} />
-        <TouchableOpacity style={styles.addButton}>
+        <TextInput
+          onChangeText={(t: string) => setText(t)}
+          value={text}
+          placeholder="Agrega nueva tarea"
+          style={styles.textInput}
+        />
+        <TouchableOpacity onPress={addTask} style={styles.addButton}>
           <Text style={styles.whiteText}>Agregar</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.scrollContainer}>
         <FlatList
           renderItem={({item}) => (
-            <RenderItem item={item} deleteFunction={deleteFunction} markDone={markDone} />
+            <RenderItem
+              item={item}
+              deleteFunction={deleteFunction}
+              markDone={markDone}
+            />
           )}
           data={tasks}
         />
